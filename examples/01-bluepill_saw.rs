d162c08a71ad3e7cbf74f2bc35a9dc1431dd0136
Mendelt Siebenga <msiebenga@gmail.com>
2019-05-15 08:44:51 +0200

    Add example from MCP4725 project

---
+++ examples/01-bluepill_saw.rs
@@ -0,0 +1,59 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use stm32f1xx_hal::delay::Delay;
+use stm32f1xx_hal::i2c::{BlockingI2c, Mode};
+use stm32f1xx_hal::pac;
+use stm32f1xx_hal::prelude::*;
+
+use mcp4725::*;
+#[allow(unused_imports)]
+use panic_semihosting;
+
+#[entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Configure the clock
+    let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    // Configure the pins for I2C1
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    // let mut delay = Delay::new(cp.SYST, clocks);
+
+    // Configure I2C
+
+    let mut i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Standard { frequency: 10000 },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    let mut dac = MCP4725::create(i2c);
+    let mut dac_cmd = Command::default();
+
+    dac_cmd = dac_cmd.address(0b00000010);
+    let mut value: u16 = 0;
+
+    loop {
+        dac_cmd = dac_cmd.data(value);
+        dac.send(&dac_cmd);
+        value += 1;
+        value &= 0x0fff;
+    }
+}
